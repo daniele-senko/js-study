e8ef5fd41459f51c9a179f57f77413b7472a802a
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-02-09 18:22:34 -0400

    Add files via upload

---
+++ Seção 5/ex_05/script.js
@@ -0,0 +1,7 @@
+let carro = 70;
+
+if (carro <= 80) {
+    console.log("Velocidade permitida");
+} else {
+    console.log("Velocidade acima do permitido");
+}
---
+++ Seção 5/ex_06/script.js
@@ -0,0 +1,8 @@
+let numero = 10;
+console.log(typeof numero);
+
+let nome = 'Maria';
+console.log(typeof nome);
+
+let number = true;
+console.log(typeof number);
