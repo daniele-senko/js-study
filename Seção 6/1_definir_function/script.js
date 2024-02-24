aef6339e3b25debd5097b876b35cec74faa85225
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-02-24 19:22:21 -0400

    Add files via upload

---
+++ Seção 6/1_definir_function/script.js
@@ -0,0 +1,21 @@
+function imprimirNoConsole() {
+    console.log('Olá, mundo!')
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+    console.log('O número é: ' + num)
+}
+
+imprimirUmNumero(5);
+imprimirUmNumero(4);
+imprimirUmNumero(3);
+imprimirUmNumero(7);
+
+const NumeroAleatorio = function() {
+    console.log(Math.random());
+}
+
+NumeroAleatorio();
+NumeroAleatorio();
