330f88211ca37bb723edca382f93e74e3bf96a08
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-03-04 14:11:54 -0400

    Add files via upload

---
+++ Seção 6/08_arrow_function _2 copy/script.js
@@ -0,0 +1,11 @@
+function soma (a, b){
+  if (a === undefined || b === undefined) {
+   console.log("Esta função precisa de dois argumentos");
+  } else {
+    return a + b;
+  }
+}
+
+console.log(soma(1));
+
+console.log(soma(1, 2));
