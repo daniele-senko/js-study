01cf48c09068834ba289c1f1b9531dff1c14291e
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-04-01 15:51:06 -0400

    criação da seção 7 e resolução do ex 16 e 17

---
+++ Seção 7/ex_17/script.js
@@ -0,0 +1,10 @@
+// escreva uma função que recebe o parâmetro de idade;
+// e imprima esta mensagem no console com template literals dizendo "você tem x anos"
+
+function imprimirIdade(age) {
+  console.log(`Você tem ${age} anos`);
+}
+
+imprimirIdade(12);
+imprimirIdade(25);
+imprimirIdade(30);
